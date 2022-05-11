98650f3cbc69b9a7bca0dadaa546fb850f46cb67
Dyrolvaid <nomellamodavid.c@gmail.com>
2022-05-11 15:00:40 +0200

    Gestiplatform v0.01: "Esqueleto" de la aplicación.

---
+++ src/main/java/com/inserta/gestiplatform/GestiplatformApplication.java
@@ -0,0 +1,13 @@
+package com.inserta.gestiplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestiplatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inserta/gestiplatform/GestiplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inserta.gestiplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
