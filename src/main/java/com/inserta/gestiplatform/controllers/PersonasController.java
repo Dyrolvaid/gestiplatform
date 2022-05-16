17d41620aacfce50ec0fa381c59b29a2bcf50330
Dyrolvaid <nomellamodavid.c@gmail.com>
2022-05-16 15:07:48 +0200

    Gestiplatform v0.25 Funcionalidad para login mejorada.

--- src/main/java/com/inserta/gestiplatform/controllers/PersonasController.java
+++ src/main/java/com/inserta/gestiplatform/controllers/PersonasController.java
@@ -4,6 +4,7 @@ import com.inserta.gestiplatform.models.Persona;
 import com.inserta.gestiplatform.repositories.PersonasRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,12 +16,13 @@ public class PersonasController {
     @Autowired
     PersonasRepo personasRepo;
     @PostMapping("/login")
-    public String login(@RequestParam String correo, @RequestParam String clave, HttpSession session){
+    public String login(@RequestParam String correo, @RequestParam String clave, HttpSession session, Model model){
         Persona usuarioEncontrado = personasRepo.findByCorreoAndClave(correo, clave);
         if(usuarioEncontrado != null){
             session.setAttribute("usuarioActivo", usuarioEncontrado);
             return "index";
         } else {
+            model.addAttribute("usuarioErroneo", false);
             return "login";
         }
     }
